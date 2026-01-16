799ed14c4a4b8841b9c0d0b3474f12a759549e6c
hactuss <hactuss@proton.me>
2026-01-16 20:49:26 +0100

    modified:   deno.lock 	modified:   package.json 	modified:   src/app.css 	modified:   src/routes/+page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,40 @@
-<main></main>
+<script>
+</script>
+
+<main>
+    <div id="window">
+        <div id="topbar">Hactuss</div>
+        <div id="content">
+            <h1>
+                <a href="https://hactuss-website.vercel.app"
+                    >redirect to https://hactuss-website.vercel.app</a
+                >
+            </h1>
+            <p></p>
+
+            <!--
+            <hr />
+            <button>darkmode</button>
+            <select name="" id="">
+                <option value="">dd</option>
+                </select>-->
+        </div>
+    </div>
+</main>
+
+<style>
+    #window {
+        width: 55%;
+        border: solid 2px black;
+        background-color: white;
+    }
+    #topbar {
+        display: flex;
+        justify-content: center;
+
+        background-color: rgb(140 255 140);
+    }
+    #content {
+        text-align: left;
+    }
+</style>
